3fd8c7edca0d515ee2eb2729129ef7621c761b7b
Retr0327 <philcoke35@gmail.com>
2022-08-18 02:24:18 +0800

    Update App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,13 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
-import { getCookie } from "cookies-next";
-import { ColorScheme } from "@mantine/core";
-import { GetServerSidePropsContext } from "next";
 import DarkModeContext from "@contexts/DarkModeContext";
 
-type CustomAppProps = AppProps & { colorScheme: ColorScheme };
-
-function App(props: CustomAppProps) {
-  const { Component, pageProps, colorScheme } = props;
-
+function App({ Component, pageProps }: AppProps) {
   return (
-    <DarkModeContext colorScheme={colorScheme}>
-      <Component {...pageProps} />;
+    <DarkModeContext>
+      <Component {...pageProps} />
     </DarkModeContext>
   );
 }
-
-App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
-  colorScheme: getCookie("mantine-color-scheme", ctx) || "light",
-});
 
 export default App;
