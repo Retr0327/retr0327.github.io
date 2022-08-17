e1747d3eb2fb07083e773eb0222f654f3797291c
Retr0327 <philcoke35@gmail.com>
2022-08-18 01:43:05 +0800

    Add ColorSchemeToggle

---
+++ src/components/ColorSchemeToggle.tsx
@@ -0,0 +1,31 @@
+import { useMemo } from "react";
+import { IconSunHigh, IconMoon } from "@tabler/icons";
+import { ActionIcon, useMantineColorScheme } from "@mantine/core";
+
+function ColorSchemeToggle() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const isDark = colorScheme === "dark";
+  const color = isDark ? "yellow" : "blue";
+
+  const icon = useMemo(
+    () =>
+      isDark ? (
+        <IconSunHigh style={{ width: 18, height: 18 }} />
+      ) : (
+        <IconMoon style={{ width: 18, height: 18 }} />
+      ),
+    [colorScheme]
+  );
+
+  return (
+    <ActionIcon
+      variant="outline"
+      color={color}
+      onClick={() => toggleColorScheme()}
+    >
+      {icon}
+    </ActionIcon>
+  );
+}
+
+export default ColorSchemeToggle;
