2f2e6395bbfea2cf520f8fdbcb0eeb62a525a371
Retr0327 <philcoke35@gmail.com>
2022-08-18 11:41:45 +0800

    Add Navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+import Navbar from "@components/Navbar";
 import DarkModeContext from "@contexts/DarkModeContext";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <DarkModeContext>
+      <Navbar />
       <Component {...pageProps} />
     </DarkModeContext>
   );
