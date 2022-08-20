196a6c0340fe99902f6d3b62d85f47d855045055
Retr0327 <philcoke35@gmail.com>
2022-08-21 02:08:38 +0800

    Add Menu

---
+++ src/components/Navbar/Menu/index.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { links } from "./links";
+import useStyles from "./styles";
+import { useState, memo } from "react";
+import { useDisclosure } from "@mantine/hooks";
+import { Group, Menu as MantineMenu, Burger } from "@mantine/core";
+
+function Menu() {
+  const { classes } = useStyles();
+  const { toggle } = useDisclosure(false)[1];
+  const [userMenuOpened, setUserMenuOpened] = useState(false);
+
+  return (
+    <MantineMenu
+      shadow="md"
+      position="bottom"
+      width="100%"
+      offset={20}
+      onOpen={() => setUserMenuOpened(true)}
+      onClose={() => setUserMenuOpened(false)}
+      classNames={classes}
+    >
+      <MantineMenu.Target>
+        <Group spacing={7}>
+          <Burger
+            opened={userMenuOpened}
+            onClick={toggle}
+            className={classes.burger}
+            size="sm"
+          />
+        </Group>
+      </MantineMenu.Target>
+      <MantineMenu.Dropdown>
+        {links.map((value) => (
+          <MantineMenu.Item key={value.label} icon={value.icon}>
+            <Link href={value.link}>
+              <a>{value.label}</a>
+            </Link>
+          </MantineMenu.Item>
+        ))}
+      </MantineMenu.Dropdown>
+    </MantineMenu>
+  );
+}
+
+export default memo(Menu);
