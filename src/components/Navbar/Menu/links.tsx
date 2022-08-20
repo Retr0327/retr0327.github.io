e14c68cce9c198adbe5ca09ca933703b1b6deb96
Retr0327 <philcoke35@gmail.com>
2022-08-21 02:16:50 +0800

    Add links

---
+++ src/components/Navbar/Menu/links.tsx
@@ -0,0 +1,19 @@
+import { IconUserCircle, IconArticle, IconHome } from "@tabler/icons";
+
+export const links = [
+  {
+    link: "/",
+    label: "Home",
+    icon: <IconHome />,
+  },
+  {
+    link: "/posts",
+    label: "Posts",
+    icon: <IconArticle />,
+  },
+  {
+    link: "/about",
+    label: "About",
+    icon: <IconUserCircle />,
+  },
+];
