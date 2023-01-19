47c9909ba4ad4f3830b058d06459ae56c6b50084
Retr0327 <philcoke35@gmail.com>
2023-01-20 02:33:17 +0800

    Update export path

--- next.config.js
+++ next.config.js
@@ -3,10 +3,17 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const debug = process.env.NODE_ENV !== 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/retr0327.github.io',
+  assetPrefix: !debug ? '/retr0327.github.io/' : '/',
+  exportPathMap: () => ({
+    '/': { page: '/' },
+    '/about': { page: '/about' },
+    '/blog': { page: '/blog' },
+  }),
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
