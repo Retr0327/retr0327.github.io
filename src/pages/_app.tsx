1036240d8228b2c42161b63d92ad0dda9aab0682
Retr0327 <philcoke35@gmail.com>
2023-01-19 17:25:24 +0800

    Reframe App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
-import { AppProps } from 'next/app';
+import { getCookie } from 'cookies-next';
+import { AppPropsWithLayout } from 'types';
+import { ColorScheme } from '@mantine/core';
+import NextApp, { AppContext } from 'next/app';
+import MantineProvider from '@contexts/MantineProvider';
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type Props = AppPropsWithLayout & { colorScheme: ColorScheme };
+
+function App(props: Props) {
+  const { Component, pageProps, colorScheme } = props;
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <MantineProvider colorScheme={colorScheme}>
+      {getLayout(<Component {...pageProps} />)}
+    </MantineProvider>
+  );
 }
+
+App.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await NextApp.getInitialProps(appContext);
+  return {
+    ...appProps,
+    colorScheme: getCookie('mantine-color-scheme', appContext.ctx) || 'light',
+  };
+};
 
 export default App;
