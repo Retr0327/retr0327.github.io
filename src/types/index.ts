a6fdea66ef49ba22322d3c048fa899869879bd9e
Retr0327 <philcoke35@gmail.com>
2023-01-19 17:27:21 +0800

    Add NextPageWithLayout type

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
+
+// -------- helpers --------
+
+export type PickAsOrNull<T, K extends keyof T> = Omit<T, K> & {
+  [P in K]: T[P] | null;
+};
+
+export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+// -------- NextJs --------
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+export type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
