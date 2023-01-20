b813cd07aec746d5d2a79f9376efee6df4b3f785
Retr0327 <philcoke35@gmail.com>
2023-01-20 15:20:17 +0800

    Add mdx path utils

---
+++ src/utils/mdx/path/index.ts
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const POSTS_PER_PAGE = 8;
+const POSTS_PATH = path.join(process.cwd(), 'src/posts');
+const TOTAL_POSTS = fs.readdirSync(POSTS_PATH).filter((file) => /\.mdx?$/.test(file));
+
+function countTotalPages() {
+  return Math.ceil(TOTAL_POSTS.length / POSTS_PER_PAGE);
+}
+
+function getSortedPosts() {
+  const posts = TOTAL_POSTS.map((filename) => ({
+    filename,
+  }));
+
+  return posts
+    .map(({ filename }) => {
+      const filePath = path.join(POSTS_PATH, filename);
+      const source = fs.readFileSync(filePath);
+      const { data, content } = matter(source);
+      const slug = filename.replace('.mdx', '');
+      return { slug, frontMatter: data, content };
+    })
+    .sort(
+      (a, b) => new Date(a.frontMatter.date).getTime() - new Date(b.frontMatter.date).getTime()
+    );
+}
+
+function getPostSlug() {
+  const postsPath = fs.readdirSync(POSTS_PATH).filter((file) => /\.mdx?$/.test(file));
+
+  return postsPath.map((filename) => {
+    const [slug] = filename.split('.');
+    return { params: { slug } };
+  });
+}
+
+export { countTotalPages, getSortedPosts, getPostSlug, POSTS_PER_PAGE, POSTS_PATH };
