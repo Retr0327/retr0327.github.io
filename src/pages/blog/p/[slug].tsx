38ca3f1f0e90c63533cb54bab88ecbfb6afc4e00
Retr0327 <philcoke35@gmail.com>
2023-01-20 15:21:37 +0800

    Add Post page

---
+++ src/pages/blog/p/[slug].tsx
@@ -0,0 +1,39 @@
+import { BlogPostProps } from 'types/blog';
+import { getPostSlug } from '@utils/mdx/path';
+import PostPage from '@components/pages/Blog/Post';
+import createMdxElements from '@utils/mdx/elements';
+import type { NextPage, GetStaticPaths, GetStaticProps } from 'next';
+
+const Post: NextPage<BlogPostProps> = (props) => {
+  const { post, frontMatter, siblings } = props;
+
+  return (
+    <article>
+      <PostPage post={post} frontMatter={frontMatter} siblings={siblings} />
+    </article>
+  );
+};
+
+export default Post;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { params } = context as { params: { slug: string } };
+  const { post, frontMatter, siblings } = await createMdxElements(params);
+
+  return {
+    props: {
+      post,
+      frontMatter,
+      siblings,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const paths = getPostSlug();
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
