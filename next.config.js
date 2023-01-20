ca2a82a83ec39ce35af4841683bfb32eab2d40a7
Retr0327 <philcoke35@gmail.com>
2023-01-20 16:17:29 +0800

    Remove assertPrefix

--- next.config.js
+++ next.config.js
@@ -3,12 +3,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProduction ? 'https://retr0327.github.io/' : '/',
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
