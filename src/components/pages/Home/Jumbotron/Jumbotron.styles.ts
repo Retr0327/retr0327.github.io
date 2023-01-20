6fcf56b5738e1cc0b21669fdb6fc55646f0c3d83
Retr0327 <philcoke35@gmail.com>
2023-01-20 19:17:52 +0800

    Add Jumbotron styles

---
+++ src/components/pages/Home/Jumbotron/Jumbotron.styles.ts
@@ -0,0 +1,41 @@
+import { createStyles } from '@mantine/core';
+
+const BREAKPOINT = '@media (max-width: 960px)';
+
+const useStyles = createStyles((theme) => ({
+  inner: {
+    position: 'relative',
+    paddingTop: 100,
+    paddingBottom: 300,
+
+    [BREAKPOINT]: {
+      paddingTop: 50,
+      paddingBottom: 100,
+    },
+  },
+
+  wrapper: {
+    padding: `${theme.spacing.xl * 2}px ${theme.spacing.xl}px`,
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: 36,
+    fontWeight: 900,
+    lineHeight: 1.1,
+    marginBottom: theme.spacing.md,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+  },
+
+  subTitle: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: 25,
+    fontWeight: 700,
+    lineHeight: 1.1,
+    marginBottom: theme.spacing.md,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+  },
+}));
+
+export default useStyles;
