10080836d3789da7bc561d711c5dcc91f9cf1c90
Retr0327 <philcoke35@gmail.com>
2023-01-20 14:35:29 +0800

    Add Blog page

---
+++ src/pages/blog/[page].tsx
@@ -0,0 +1,38 @@
+import { BlogGalleryProps } from 'types/blog';
+import BlogLayout from '@components/layout/Blog';
+import BlogGallery from '@components/pages/Blog/Gallery';
+import { NextPage, GetStaticProps, GetStaticPaths } from 'next';
+import { countTotalPages, getSortedPosts, POSTS_PER_PAGE } from '@utils/mdx/path';
+
+const Blog: NextPage<BlogGalleryProps> = (props) => (
+  <BlogLayout>
+    <BlogGallery {...props} />
+  </BlogLayout>
+);
+
+export default Blog;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { params } = context as { params: { page: string } };
+  const posts = getSortedPosts();
+  const pageIndex = Number(params.page) - 1;
+  const startIndex = pageIndex * POSTS_PER_PAGE;
+  const endIndex = (pageIndex + 1) * POSTS_PER_PAGE;
+  const totalPages = countTotalPages();
+
+  return {
+    props: {
+      posts: posts.slice(startIndex, endIndex),
+      totalPages,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const totalPages = countTotalPages();
+  const paths = [...Array(totalPages)].map((v, i) => ({
+    params: { page: (i + 1).toString() },
+  }));
+
+  return { paths, fallback: false };
+};
--- src/pages/blog/index.tsx
+++
@@ -1,5 +0,0 @@
-import { NextPage } from 'next';
-
-const Blog: NextPage = () => <>blog page</>;
-
-export default Blog;
