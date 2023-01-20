c9af04768c51bd0547477ce3ff47ab2b9ae1dc60
Retr0327 <philcoke35@gmail.com>
2023-01-20 16:21:09 +0800

    Add exportPathMap

--- next.config.js
+++ next.config.js
@@ -3,9 +3,17 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProduction ? 'https://retr0327.github.io/' : '/',
+  exportPathMap: () => ({
+    '/': { page: '/' },
+    '/about': { page: '/about' },
+    '/blog': { page: '/blog' },
+  }),
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
