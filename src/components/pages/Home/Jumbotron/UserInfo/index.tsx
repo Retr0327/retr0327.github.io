9b4ee1232a28dec57a4510d07856654c8388ae50
Retr0327 <philcoke35@gmail.com>
2023-01-21 00:14:06 +0800

    Add CV ActionIcon

--- src/components/pages/Home/Jumbotron/UserInfo/index.tsx
+++ src/components/pages/Home/Jumbotron/UserInfo/index.tsx
@@ -1,8 +1,7 @@
 import { memo } from 'react';
-import { IconMail } from '@tabler/icons';
 import layoutConfig from '@config/layout';
-import IconController from '@components/common/Icons';
-import { Avatar, Text, Box, Group, ActionIcon } from '@mantine/core';
+import { Avatar, Text, Box, Group } from '@mantine/core';
+import { Github, LinkedIn, Email, CV } from './ActionIcon';
 
 function UserInfo() {
   return (
@@ -16,49 +15,10 @@ function UserInfo() {
       </Text>
 
       <Group spacing={10} position="center" mt="lg">
-        <ActionIcon
-          variant="default"
-          radius="md"
-          size={36}
-          sx={(theme) => ({
-            color: theme.white,
-            backgroundImage: 'linear-gradient(90deg, #fd7e14 0%, #fa5252 100%)',
-          })}
-          component="a"
-          href="mailto:lixingyang.dev@gmail.com"
-        >
-          <IconMail size={20} />
-        </ActionIcon>
-        <ActionIcon
-          variant="default"
-          radius="md"
-          size={36}
-          sx={(theme) => ({
-            color: theme.white,
-            backgroundColor: layoutConfig.linkedin.color,
-            borderColor: layoutConfig.linkedin.color,
-            ...theme.fn.hover({
-              backgroundColor: theme.fn.lighten(layoutConfig.linkedin.color, 0.1),
-            }),
-          })}
-          component="a"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={layoutConfig.linkedin.link}
-        >
-          <IconController size={18} control="linkedin" />
-        </ActionIcon>
-        <ActionIcon
-          variant="default"
-          radius="md"
-          size={36}
-          component="a"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={layoutConfig.github.lixing}
-        >
-          <IconController size={18} control="github" />
-        </ActionIcon>
+        <CV />
+        <Email />
+        <LinkedIn />
+        <Github />
       </Group>
     </Box>
   );
