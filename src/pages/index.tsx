89a11c47f99bca1dea58b4a115421a6f8b3c7f7e
Retr0327 <philcoke35@gmail.com>
2023-01-20 19:18:46 +0800

    Update Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import { Box } from '@mantine/core';
 import type { NextPage } from 'next';
+import Footer from '@components/common/Footer';
+import Jumbotron from '@components/pages/Home/Jumbotron';
 
-const Home: NextPage = () => <>This is homepage</>;
+const Home: NextPage = () => (
+  <>
+    <Box sx={{ position: 'relative', zIndex: 1 }}>
+      <Jumbotron />
+    </Box>
+    <Footer />
+  </>
+);
 
 export default Home;
