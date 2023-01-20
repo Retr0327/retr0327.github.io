09ab13d2417fc6f10bb7cd9b9b9ce28a116160bf
Retr0327 <philcoke35@gmail.com>
2023-01-20 16:13:42 +0800

    Add avatar link

--- src/config/layout.ts
+++ src/config/layout.ts
@@ -1,4 +1,6 @@
 const layoutConfig = {
+  avatar:
+    'https://scontent.fkhh1-2.fna.fbcdn.net/v/t1.6435-9/90348802_2879430425476831_5109053951542558720_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=soyfcAOpwxYAX_Gmt-4&_nc_ht=scontent.fkhh1-2.fna&oh=00_AfCuvvtDME6K7RQX33SrhEWClPrrBA_IPdJeFe-Uac9SZw&oe=63F1D1FA',
   cv: 'https://docs.google.com/document/d/1hiYT_QK5draFMMogwfwf5H2coHcJDveS/edit?usp=share_link&ouid=115833122300012543204&rtpof=true&sd=true',
   linkedin: {
     color: '#0A66C2',
