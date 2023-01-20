a76146e84667bf817dc8b24b940cea2e27e5dd5d
Retr0327 <philcoke35@gmail.com>
2023-01-20 15:18:06 +0800

    Add BlogPostProps

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,6 +1,27 @@
-import { Frontmatter } from 'types/mdx';
+import { MDXRemoteSerializeResult } from 'next-mdx-remote';
 
 export type BlogGalleryProps = {
   posts: { slug: string; frontMatter: Frontmatter }[];
   totalPages: number;
 };
+
+export type BlogPostProps = {
+  post: {
+    source: MDXRemoteSerializeResult;
+    headings: { depth: number; value: string }[];
+  };
+  frontMatter: Frontmatter;
+  siblings: {
+    next: Frontmatter | null;
+    prev: Frontmatter | null;
+  };
+};
+
+export interface Frontmatter {
+  title: string;
+  date: string;
+  slug: string;
+  excerpt: string;
+  category: string[];
+  coverImage?: string;
+}
