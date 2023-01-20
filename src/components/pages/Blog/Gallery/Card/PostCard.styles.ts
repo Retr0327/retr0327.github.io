d85ff0dd2a09efc59c74c8490797fb3924afcc67
Retr0327 <philcoke35@gmail.com>
2023-01-20 14:33:11 +0800

    Add PostCard styles

---
+++ src/components/pages/Blog/Gallery/Card/PostCard.styles.ts
@@ -0,0 +1,25 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    position: 'relative',
+    width: '80%',
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+
+    '@media (max-width: 420px)': {
+      width: '100%',
+    },
+  },
+
+  title: {
+    display: 'block',
+    marginTop: theme.spacing.md,
+    marginBottom: theme.spacing.xs / 2,
+  },
+
+  footer: {
+    marginTop: theme.spacing.md,
+  },
+}));
+
+export default useStyles;
