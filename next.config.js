5a08112544a8df6a42cdc91d6e348599ac9331a1
Retr0327 <philcoke35@gmail.com>
2023-01-20 16:39:48 +0800

    Update path map

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -9,11 +12,31 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   assetPrefix: isProduction ? 'https://retr0327.github.io/' : '/',
-  exportPathMap: () => ({
-    '/': { page: '/' },
-    '/about': { page: '/about' },
-    '/blog': { page: '/blog' },
-  }),
+  exportPathMap: (defaultPathMap, ctx) => {
+    const pathMap = {
+      ...defaultPathMap,
+      '/blog/[page]': {
+        page: '/blog',
+        query: { page: '[page]' },
+      },
+      '/blog/p/[slug]': {
+        page: '/blog/p',
+        query: { slug: '[slug]' },
+      },
+    };
+
+    const files = fs.readdirSync(path.join(ctx.dir, 'src/posts'));
+
+    files.forEach((file) => {
+      const slug = file.replace('.mdx', '');
+      pathMap[`/blog/p/${slug}`] = {
+        page: '/blog/p',
+        query: { slug },
+      };
+    });
+
+    return pathMap;
+  },
   images: { unoptimized: true },
 };
 
