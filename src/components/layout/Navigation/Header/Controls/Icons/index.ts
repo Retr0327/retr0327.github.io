9075356e2b2f716466a3867fee88ed620787e827
Retr0327 <philcoke35@gmail.com>
2023-01-21 01:42:25 +0800

    Add LinkedIn

--- src/components/layout/Navigation/Header/Controls/Icons/index.ts
+++
@@ -1,6 +0,0 @@
-import Github from './Github';
-import LinkedIn from './LinkedIn';
-import Facebook from './Facebook';
-import ColorScheme from './ColorScheme';
-
-export { Github, LinkedIn, Facebook, ColorScheme };
