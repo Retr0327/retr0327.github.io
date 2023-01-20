7d3ed617733f05a57bf1b581ab147185f45b6f67
Retr0327 <philcoke35@gmail.com>
2023-01-20 16:49:02 +0800

    Remove incorrect memo

--- src/components/pages/Blog/Gallery/Card/index.tsx
+++ src/components/pages/Blog/Gallery/Card/index.tsx
@@ -1,4 +1,4 @@
-import { memo, useMemo } from 'react';
+import { memo } from 'react';
 import { BlogGalleryProps } from 'types/blog';
 import { Card, Stack, Image, Badge, Text, Group } from '@mantine/core';
 import Link from 'next/link';
@@ -10,47 +10,42 @@ function PostCard(props: Props) {
   const { posts } = props;
   const { classes } = useStyles();
 
-  const items = useMemo(
-    () =>
-      posts.map(({ frontMatter }, index) => (
-        <Card key={`${frontMatter.title}-${index}`} withBorder radius="md" className={classes.card}>
-          <Card.Section>
-            <Link href={frontMatter.slug}>
-              <Image src={frontMatter.coverImage} height={300} />
-            </Link>
-          </Card.Section>
+  const items = posts.map(({ frontMatter }, index) => (
+    <Card key={`${frontMatter.title}-${index}`} withBorder radius="md" className={classes.card}>
+      <Card.Section>
+        <Link href={frontMatter.slug}>
+          <Image src={frontMatter.coverImage} height={300} />
+        </Link>
+      </Card.Section>
 
-          <Text className={classes.title} weight={500} component={Link} href={frontMatter.slug}>
-            {frontMatter.title}
-          </Text>
+      <Text className={classes.title} weight={500} component={Link} href={frontMatter.slug}>
+        {frontMatter.title}
+      </Text>
 
-          <Text size="sm" color="dimmed" lineClamp={4}>
-            {frontMatter.excerpt}
-          </Text>
+      <Text size="sm" color="dimmed" lineClamp={4}>
+        {frontMatter.excerpt}
+      </Text>
 
-          <Group position="apart" className={classes.footer}>
-            <Text size="sm" color="dimmed">
-              發表於 {frontMatter.createdAt}
-              {frontMatter.updatedAt !== undefined ? ` | 更新於 ${frontMatter.updatedAt}` : null}
-            </Text>
+      <Group position="apart" className={classes.footer}>
+        <Text size="sm" color="dimmed">
+          發表於 {frontMatter.createdAt}
+          {frontMatter.updatedAt !== undefined ? ` | 更新於 ${frontMatter.updatedAt}` : null}
+        </Text>
 
-            <Group spacing={8} mr={0}>
-              {frontMatter.category.map((category, categoryIndex) => (
-                <Badge
-                  key={`${category}-${categoryIndex}`}
-                  variant="gradient"
-                  gradient={{ from: 'indigo', to: 'cyan' }}
-                >
-                  {category}
-                </Badge>
-              ))}
-            </Group>
-          </Group>
-        </Card>
-      )),
-    [posts]
-  );
-
+        <Group spacing={8} mr={0}>
+          {frontMatter.category.map((category, categoryIndex) => (
+            <Badge
+              key={`${category}-${categoryIndex}`}
+              variant="gradient"
+              gradient={{ from: 'indigo', to: 'cyan' }}
+            >
+              {category}
+            </Badge>
+          ))}
+        </Group>
+      </Group>
+    </Card>
+  ));
   return (
     <Stack align="center" sx={{ width: '100%' }} spacing={50}>
       {items}
