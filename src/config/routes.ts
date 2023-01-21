d4e427b8e937b3b4cedc2bcdeaac187b54030408
Retr0327 <philcoke35@gmail.com>
2023-01-21 15:45:35 +0800

    Add header and menu links

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,6 +1,21 @@
+import { LinkItem, PartialBy } from 'types';
+import { TablerIcon, IconHome, IconArticle } from '@tabler/icons';
+
 const Route = {
   home: '/',
   blog: '/blog',
 } as const;
+
+type MenuLink = PartialBy<LinkItem, 'link'> & { icon: TablerIcon };
+
+const home = { label: 'Home', link: Route.home };
+const blog = { label: 'Blog', link: `${Route.blog}/1` };
+
+export const headerLinks: LinkItem[] = [home, blog];
+
+export const menuLinks: MenuLink[] = [
+  { ...home, icon: IconHome },
+  { ...blog, icon: IconArticle },
+];
 
 export default Route;
