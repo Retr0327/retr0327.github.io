a3c8d5d8247b0576c406105a43ab7e3526067d25
Retr0327 <philcoke35@gmail.com>
2023-01-21 13:55:00 +0800

    Add wrapper

--- src/components/layout/Blog/BlogLayout.styles.ts
+++ src/components/layout/Blog/BlogLayout.styles.ts
@@ -2,6 +2,14 @@ import layoutConfig from '@config/layout';
 import { createStyles } from '@mantine/core';
 
 const useStyles = createStyles((theme) => ({
+  wrapper: {
+    paddingLeft: layoutConfig.navbar.width,
+
+    [`@media (max-width: ${layoutConfig.navbar.breakpoint}px)`]: {
+      paddingLeft: 0,
+    },
+  },
+
   content: {
     minHeight: 'calc(100vh - 350px)',
     position: 'relative',
@@ -9,21 +17,6 @@ const useStyles = createStyles((theme) => ({
     backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
     boxShadow: theme.colorScheme === 'dark' ? 'none' : theme.shadows.sm,
     paddingBottom: 80,
-  },
-
-  wrapper: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
-    zIndex: 1,
-    display: 'flex',
-    position: 'relative',
-    justifyContent: 'space-between',
-    paddingLeft: theme.spacing.xl * 2,
-    paddingRight: theme.spacing.xl * 2,
-
-    [`@media (max-width: ${layoutConfig.mdx.breakpoint}px)`]: {
-      paddingLeft: theme.spacing.xl,
-      paddingRight: theme.spacing.xl,
-    },
   },
 }));
 
