1dc1f98b12229a62d232a4c5a1e7d26654d67153
Retr0327 <philcoke35@gmail.com>
2023-01-21 18:03:50 +0800

    Reframe Navbar

--- src/components/common/Navigation/Navbar/index.tsx
+++ src/components/common/Navigation/Navbar/index.tsx
@@ -1,17 +1,26 @@
 import { memo } from 'react';
+import { Frontmatter } from 'types/blog';
+import { IconHome } from '@tabler/icons';
 import { useSpotlight } from '@mantine/spotlight';
+import Route, { createBlogLinks } from '@config/routes';
 import { Navbar as MantineNavbar } from '@mantine/core';
+import NavItems from './NavItem';
 import SearchControl from './Control';
 import useStyles from './Navbar.styles';
 
-function Navbar() {
+type Props = { posts: { frontMatter: Frontmatter }[] };
+
+function Navbar(props: Props) {
   const { classes } = useStyles();
   const spotlight = useSpotlight();
+  const { posts } = props;
+  const links = createBlogLinks(posts);
 
   return (
     <MantineNavbar className={classes.navbar}>
-      <MantineNavbar.Section className={classes.mainSection} grow>
-        <SearchControl onClick={spotlight.openSpotlight} sx={{ width: '100%' }} />
+      <MantineNavbar.Section className={classes.section}>
+        <SearchControl onClick={spotlight.openSpotlight} sx={{ width: '100%' }} mb={20} />
+        <NavItems links={[{ label: 'Home', link: Route.home, icon: IconHome }, ...links]} />
       </MantineNavbar.Section>
     </MantineNavbar>
   );
