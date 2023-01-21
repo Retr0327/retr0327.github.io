b31665478537c160ec09278264f7c40fbe2f59b3
Retr0327 <philcoke35@gmail.com>
2023-01-21 18:02:05 +0800

    Reframe BlogLayout

--- src/components/layout/Blog/index.tsx
+++ src/components/layout/Blog/index.tsx
@@ -1,48 +1,74 @@
+import Link from 'next/link';
 import { memo, ReactNode } from 'react';
+import { Frontmatter } from 'types/blog';
 import { IconSearch } from '@tabler/icons';
-import { headerLinks } from '@config/routes';
 import Footer from '@components/common/Footer';
 import { useSpotlight } from '@mantine/spotlight';
 import Menu from '@components/common/Navigation/Menu';
-import { Box, Menu as MantineMenu } from '@mantine/core';
-import Header from '@components/common/Navigation/Header';
 import Navbar from '@components/common/Navigation/Navbar';
+import Header from '@components/common/Navigation/Header';
+import { headerLinks, createBlogLinks } from '@config/routes';
 import createMenuItems from '@components/common/Navigation/Menu/Items';
 import SearchControl from '@components/common/Navigation/Navbar/Control';
+import { ActionIcon, Badge, Box, Menu as MantineMenu } from '@mantine/core';
 import useStyles from './BlogLayout.styles';
 import useNavigationLayoutStyles from '../NavigationLayout.styles';
 
 type Props = {
   children: ReactNode;
+  posts: { frontMatter: Frontmatter }[];
 };
 
 function BlogLayout(props: Props) {
-  const { children } = props;
+  const { posts, children } = props;
   const { classes } = useStyles();
   const menuItems = createMenuItems();
   const spotlight = useSpotlight();
   const { classes: navigatoinClasses } = useNavigationLayoutStyles();
+  const blogLinks = createBlogLinks(posts);
 
   return (
     <>
       <Header
         links={headerLinks}
-        searchControl={<SearchControl onClick={spotlight.openSpotlight} />}
+        searchControl={
+          <>
+            <SearchControl onClick={spotlight.openSpotlight} />
+            <ActionIcon
+              sx={(theme) => ({
+                [theme.fn.largerThan('sm')]: {
+                  display: 'none',
+                },
+              })}
+              onClick={spotlight.openSpotlight}
+            >
+              <IconSearch size={14} stroke={1.5} />
+            </ActionIcon>
+          </>
+        }
       >
         <Menu
           menuItems={[
             ...menuItems,
-            <MantineMenu.Item
-              key="spotlight"
-              onClick={spotlight.openSpotlight}
-              icon={<IconSearch size={14} stroke={1.5} />}
-            >
-              Search
-            </MantineMenu.Item>,
+            ...blogLinks.map((blogLink, index) => (
+              <MantineMenu.Item
+                key={`${blogLink.label}-${index}`}
+                component={Link}
+                href={blogLink.link!}
+                icon={<blogLink.icon size={14} stroke={1.5} />}
+                rightSection={
+                  <Badge size="sm" variant="filled" className={classes.mainLinkBadge}>
+                    {blogLink.count}
+                  </Badge>
+                }
+              >
+                {blogLink.label}
+              </MantineMenu.Item>
+            )),
           ]}
         />
       </Header>
-      <Navbar />
+      <Navbar posts={posts} />
       <main className={navigatoinClasses.main}>
         <div className={navigatoinClasses.content}>
           <Box className={classes.wrapper}>
