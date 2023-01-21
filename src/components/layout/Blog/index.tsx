eaa95608741a2dbcd7fe7744aafc8e0b22697a2a
Retr0327 <philcoke35@gmail.com>
2023-01-21 12:45:48 +0800

    Reframe BlogLayout

--- src/components/layout/Blog/index.tsx
+++ src/components/layout/Blog/index.tsx
@@ -1,20 +1,30 @@
 import dynamic from 'next/dynamic';
 import { Box } from '@mantine/core';
 import { memo, ReactNode } from 'react';
+import { Frontmatter } from 'types/blog';
+import SpotlightProvider from '@contexts/Spotlight';
+import Navbar from '../Navigation/Navbar';
 import useStyles from './BlogLayout.styles';
 
 const Footer = dynamic(() => import('@components/common/Footer'));
 
-function BlogLayout({ children }: { children: ReactNode }) {
+type Props = {
+  children: ReactNode;
+  posts: { frontMatter: Frontmatter }[];
+};
+
+function BlogLayout(props: Props) {
+  const { posts, children } = props;
   const { classes } = useStyles();
 
   return (
-    <>
+    <SpotlightProvider posts={posts}>
+      <Navbar />
       <Box className={classes.content}>
-        <Box className={classes.wrapper}>{children}</Box>
+        <Box>{children}</Box>
       </Box>
       <Footer withNavbar />
-    </>
+    </SpotlightProvider>
   );
 }
 
