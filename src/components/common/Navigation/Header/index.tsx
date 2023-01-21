c600306eed4f0c745ffbc5d3afd879911c4441c7
Retr0327 <philcoke35@gmail.com>
2023-01-21 16:07:12 +0800

    Add Header

---
+++ src/components/common/Navigation/Header/index.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import { LinkItem } from 'types';
+import { memo, ReactNode } from 'react';
+import { Header as MantineHeader, Group, Text } from '@mantine/core';
+import ColorSchemeToggle from '@components/common/ColorSchemeToggle';
+import NavItems from './NavItem';
+import HeaderControls from './Controls';
+import useStyles from './Header.styles';
+
+type Props = {
+  links: LinkItem[];
+  children: ReactNode;
+};
+
+function Header(props: Props) {
+  const { links, children } = props;
+  const { classes } = useStyles();
+
+  return (
+    <MantineHeader height={60} className={classes.header}>
+      {children}
+      <Group>
+        <div className={classes.logoWrapper}>
+          <Link href="/" className={classes.logo}>
+            <Text mb={5}> Lixing Yang </Text>
+          </Link>
+        </div>
+      </Group>
+      <Group>
+        <Group spacing={5} className={classes.links}>
+          <NavItems links={links} />
+        </Group>
+        <HeaderControls />
+        <ColorSchemeToggle />
+      </Group>
+    </MantineHeader>
+  );
+}
+
+export default memo(Header);
