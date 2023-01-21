292630556befa8f43aa93e80578cf27807548f75
Retr0327 <philcoke35@gmail.com>
2023-01-21 16:05:26 +0800

    Add BlogLayout

--- src/components/layout/Blog/index.tsx
+++ src/components/layout/Blog/index.tsx
@@ -1,32 +1,53 @@
-import dynamic from 'next/dynamic';
-import { Box } from '@mantine/core';
 import { memo, ReactNode } from 'react';
-import { Frontmatter } from 'types/blog';
-import SpotlightProvider from '@contexts/Spotlight';
-import Navbar from '../Navigation/Navbar';
+import { IconSearch } from '@tabler/icons';
+import { headerLinks } from '@config/routes';
+import Footer from '@components/common/Footer';
+import { useSpotlight } from '@mantine/spotlight';
+import Menu from '@components/common/Navigation/Menu';
+import { Box, Menu as MantineMenu } from '@mantine/core';
+import Header from '@components/common/Navigation/Header';
+import Navbar from '@components/common/Navigation/Navbar';
+import createMenuItems from '@components/common/Navigation/Menu/Items';
 import useStyles from './BlogLayout.styles';
-
-const Footer = dynamic(() => import('@components/common/Footer'));
+import useNavigationLayoutStyles from '../NavigationLayout.styles';
 
 type Props = {
   children: ReactNode;
-  posts: { frontMatter: Frontmatter }[];
 };
 
 function BlogLayout(props: Props) {
-  const { posts, children } = props;
+  const { children } = props;
   const { classes } = useStyles();
+  const menuItems = createMenuItems();
+  const spotlight = useSpotlight();
+  const { classes: navigatoinClasses } = useNavigationLayoutStyles();
 
   return (
-    <SpotlightProvider posts={posts}>
-      <Box className={classes.wrapper}>
-        <Navbar />
-        <Box className={classes.content}>
-          <Box>{children}</Box>
-        </Box>
-      </Box>
-      <Footer withNavbar />
-    </SpotlightProvider>
+    <>
+      <Header links={headerLinks}>
+        <Menu
+          menuItems={[
+            ...menuItems,
+            <MantineMenu.Item
+              key="spotlight"
+              onClick={spotlight.openSpotlight}
+              icon={<IconSearch size={14} stroke={1.5} />}
+            >
+              Search
+            </MantineMenu.Item>,
+          ]}
+        />
+      </Header>
+      <Navbar />
+      <main className={navigatoinClasses.main}>
+        <div className={navigatoinClasses.content}>
+          <Box className={classes.wrapper}>
+            <Box className={classes.content}>{children}</Box>
+          </Box>
+          <Footer withNavbar />
+        </div>
+      </main>
+    </>
   );
 }
 
