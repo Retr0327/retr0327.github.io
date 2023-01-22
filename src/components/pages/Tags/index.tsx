90ef4733e1c2e6e4ddb662d8cb84f424e2853477
Retr0327 <philcoke35@gmail.com>
2023-01-23 01:13:18 +0800

    Add TagsPage

---
+++ src/components/pages/Tags/index.tsx
@@ -0,0 +1,21 @@
+import { memo } from 'react';
+import { BlogGalleryProps } from 'types/blog';
+import countCategories from '@utils/mdx/category';
+import TagCloud from './Cloud';
+import useStyles from './TagsPage.styles';
+
+type Props = Pick<BlogGalleryProps, 'posts'>;
+
+function TagsPage(props: Props) {
+  const { posts } = props;
+  const { classes } = useStyles();
+  const categoryCounts = countCategories(posts);
+
+  return (
+    <div className={classes.container}>
+      <TagCloud data={categoryCounts} />
+    </div>
+  );
+}
+
+export default memo(TagsPage);
