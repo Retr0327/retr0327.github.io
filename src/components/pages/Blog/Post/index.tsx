d68bcb371ef6fdbc1063827d337b2cd54bc61c98
Retr0327 <philcoke35@gmail.com>
2023-01-22 15:21:39 +0800

    Add MdxHeader

--- src/components/pages/Blog/Post/index.tsx
+++ src/components/pages/Blog/Post/index.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react';
 import { BlogPostProps } from 'types/blog';
 import { MDXRemote } from 'next-mdx-remote';
 import MdxSiblings from './MdxSiblings';
+import MdxHeader from './MdxHeader';
 import { components } from './Components';
 import useStyles from './PostPage.styles';
 import TableOfContents from './TableOfContents';
@@ -9,13 +10,14 @@ import TableOfContents from './TableOfContents';
 type Props = Omit<BlogPostProps, 'posts'>;
 
 function PostPage(props: Props) {
-  const { post, siblings } = props;
+  const { post, siblings, frontMatter } = props;
   const { classes } = useStyles();
   const { source, headings } = post;
 
   return (
     <div className={classes.wrapper}>
       <div className={classes.container}>
+        <MdxHeader frontMatter={frontMatter} />
         <MDXRemote {...source} components={components} lazy />
         <MdxSiblings siblings={siblings} />
       </div>
