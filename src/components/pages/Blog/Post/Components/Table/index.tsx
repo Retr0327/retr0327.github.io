2ab417af31f63b9aa3574716bc6e62205b91e62a
Retr0327 <philcoke35@gmail.com>
2023-01-24 02:46:00 +0800

    Update Table width in small screen

--- src/components/pages/Blog/Post/Components/Table/index.tsx
+++ src/components/pages/Blog/Post/Components/Table/index.tsx
@@ -4,7 +4,18 @@ import { ScrollArea, Table as MantineTable } from '@mantine/core';
 function Table(props: any) {
   return (
     <ScrollArea type="scroll">
-      <MantineTable withBorder withColumnBorders {...props} sx={{ width: '50%' }} />
+      <MantineTable
+        withBorder
+        withColumnBorders
+        {...props}
+        sx={(theme) => ({
+          width: '50%',
+
+          [theme.fn.smallerThan('sm')]: {
+            width: '100%',
+          },
+        })}
+      />
     </ScrollArea>
   );
 }
