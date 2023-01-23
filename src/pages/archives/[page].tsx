3bff72cc4bab72972c56a2e524c6ff68bc73b790
Retr0327 <philcoke35@gmail.com>
2023-01-23 13:58:45 +0800

    Use `TOTAL_POSTS` to generate page number

--- src/pages/archives/[page].tsx
+++ src/pages/archives/[page].tsx
@@ -5,7 +5,7 @@ import BlogLayout from '@components/layout/Blog';
 import SpotlightProvider from '@contexts/Spotlight';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import ArchivesPage from '@components/pages/Archives';
-import { countTotalPages, getSortedPosts } from '@utils/mdx/path';
+import { countTotalPages, getSortedPosts, TOTAL_POSTS } from '@utils/mdx/path';
 
 const POSTS_PER_PAGE = 10;
 
@@ -29,7 +29,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const pageIndex = Number(params.page) - 1;
   const startIndex = pageIndex * POSTS_PER_PAGE;
   const endIndex = (pageIndex + 1) * POSTS_PER_PAGE;
-  const totalPages = countTotalPages(POSTS_PER_PAGE);
+  const totalPages = countTotalPages(TOTAL_POSTS, POSTS_PER_PAGE);
 
   return {
     props: {
@@ -41,7 +41,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 };
 
 export const getStaticPaths: GetStaticPaths = () => {
-  const totalPages = countTotalPages(POSTS_PER_PAGE);
+  const totalPages = countTotalPages(TOTAL_POSTS, POSTS_PER_PAGE);
   const paths = [...Array(totalPages)].map((_, i) => ({
     params: { page: (i + 1).toString() },
   }));
--- src/pages/blog/[page].tsx
+++ src/pages/blog/[page].tsx
@@ -5,7 +5,7 @@ import { NextPageWithLayout } from 'types/index';
 import SpotlightProvider from '@contexts/Spotlight';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import BlogGallery from '@components/pages/Blog/Gallery';
-import { countTotalPages, getSortedPosts } from '@utils/mdx/path';
+import { countTotalPages, getSortedPosts, TOTAL_POSTS } from '@utils/mdx/path';
 
 const POSTS_PER_PAGE = 8;
 
@@ -29,7 +29,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const pageIndex = Number(params.page) - 1;
   const startIndex = pageIndex * POSTS_PER_PAGE;
   const endIndex = (pageIndex + 1) * POSTS_PER_PAGE;
-  const totalPages = countTotalPages(POSTS_PER_PAGE);
+  const totalPages = countTotalPages(TOTAL_POSTS, POSTS_PER_PAGE);
 
   return {
     props: {
@@ -41,7 +41,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 };
 
 export const getStaticPaths: GetStaticPaths = () => {
-  const totalPages = countTotalPages(POSTS_PER_PAGE);
+  const totalPages = countTotalPages(TOTAL_POSTS, POSTS_PER_PAGE);
   const paths = [...Array(totalPages)].map((v, i) => ({
     params: { page: (i + 1).toString() },
   }));
