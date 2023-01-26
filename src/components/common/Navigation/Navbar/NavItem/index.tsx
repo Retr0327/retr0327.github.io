5ca1826a1b30d60a47ca9f526ad2743c2238030e
Retr0327 <philcoke35@gmail.com>
2023-01-27 01:27:24 +0800

    Highlight selected nav item

--- src/components/common/Navigation/Navbar/NavItem/index.tsx
+++ src/components/common/Navigation/Navbar/NavItem/index.tsx
@@ -1,29 +1,38 @@
 import Link from 'next/link';
 import { NavLink } from 'types';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { getRouteFromPathname } from '@utils';
 import { Badge, UnstyledButton } from '@mantine/core';
 import useStyles from './NavItems.styles';
 
 type Props = { links: NavLink[] };
 
 function NavItems(props: Props) {
-  const { classes } = useStyles();
+  const { classes, cx } = useStyles();
   const { links } = props;
+  const { pathname } = useRouter();
+  const route = getRouteFromPathname(pathname);
+  const [active, setActive] = useState(route);
 
   return (
     <>
       {links.map((link) => (
         <UnstyledButton
           key={link.label}
-          className={classes.mainLink}
+          className={cx(classes.link, {
+            [classes.linkActive]: getRouteFromPathname(link.link!) === active,
+          })}
           component={Link}
           href={link.link!}
+          onClick={() => setActive(getRouteFromPathname(link.link!))}
         >
-          <div className={classes.mainLinkInner}>
-            <link.icon size={20} className={classes.mainLinkIcon} stroke={1.5} />
+          <div className={classes.linkInner}>
+            <link.icon size={20} className={classes.linkIcon} stroke={1.5} />
             <span>{link.label}</span>
           </div>
           {link.count && (
-            <Badge size="sm" variant="filled" className={classes.mainLinkBadge}>
+            <Badge size="sm" variant="filled" className={classes.badge}>
               {link.count}
             </Badge>
           )}
