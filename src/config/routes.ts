6bfcba3ebad63346e027fc57d9506ff1f4e6c6c6
Retr0327 <philcoke35@gmail.com>
2023-01-27 00:52:23 +0800

    Remove categories route

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,14 +1,13 @@
 import { countCategories } from '@utils/mdx';
 import { FrontMatterProps } from 'types/blog';
 import { MenuLink, NavLink, LinkItem } from 'types';
-import { IconArchive, IconHome, IconArticle, IconTag, IconCategory } from '@tabler/icons';
+import { IconArchive, IconHome, IconArticle, IconTag } from '@tabler/icons';
 
 const Route = {
   home: '/',
   blog: '/blog',
   tags: '/tags',
   archives: '/archives',
-  categories: '/categories',
   notFound: '/404',
 } as const;
 
@@ -28,7 +27,6 @@ export function createBlogLinks(posts: FrontMatterProps[]): NavLink[] {
 
   return [
     { label: 'Tags', link: Route.tags, icon: IconTag, count: size },
-    { label: 'Category', link: Route.categories, icon: IconCategory, count: size },
     { label: 'Archives', link: `${Route.archives}/1`, icon: IconArchive, count: posts.length },
   ];
 }
