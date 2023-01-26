bf228c56211aadcca5e70accfa3d346aecd49a4b
Retr0327 <philcoke35@gmail.com>
2023-01-26 18:26:52 +0800

    Implement category counting function

--- src/utils/mdx/index.ts
+++ src/utils/mdx/index.ts
@@ -1,10 +1,10 @@
 import { Frontmatter } from 'types/blog';
 
-type Posts = {
-  frontMatter: Frontmatter;
-}[];
-
-function countCategories(posts: Posts) {
+function countCategories(
+  posts: {
+    frontMatter: Frontmatter;
+  }[]
+) {
   return posts.reduce((acc: { [key: string]: number }, curr) => {
     curr.frontMatter.category.forEach((category) => {
       acc[category] = (acc[category] || 0) + 1;
