961abacab84421394401bf594bcf615477bf82ae
Retr0327 <philcoke35@gmail.com>
2023-01-27 01:27:11 +0800

    Add linkActive

--- src/components/common/Navigation/Navbar/NavItem/NavItems.styles.ts
+++ src/components/common/Navigation/Navbar/NavItem/NavItems.styles.ts
@@ -1,39 +1,59 @@
 import { createStyles } from '@mantine/core';
 
-const useStyles = createStyles((theme) => ({
-  mainLink: {
-    display: 'flex',
-    alignItems: 'center',
-    width: '100%',
-    fontSize: theme.fontSizes.xs,
-    padding: `8px ${theme.spacing.xs}px`,
-    borderRadius: theme.radius.sm,
-    fontWeight: 500,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+const useStyles = createStyles((theme, _params, getRef) => {
+  const icon = getRef('icon');
 
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+  return {
+    link: {
+      display: 'flex',
+      alignItems: 'center',
+      width: '100%',
+      fontSize: theme.fontSizes.xs,
+      padding: `8px ${theme.spacing.xs}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+        [`& .${icon}`]: {
+          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        },
+      },
     },
-  },
 
-  mainLinkInner: {
-    display: 'flex',
-    alignItems: 'center',
-    flex: 1,
-  },
+    linkActive: {
+      '&, &:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
 
-  mainLinkIcon: {
-    marginRight: theme.spacing.sm,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
-  },
+        [`& .${icon}`]: {
+          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        },
+      },
+    },
 
-  mainLinkBadge: {
-    padding: 0,
-    width: 20,
-    height: 20,
-    pointerEvents: 'none',
-  },
-}));
+    linkInner: {
+      display: 'flex',
+      alignItems: 'center',
+      flex: 1,
+    },
+
+    linkIcon: {
+      ref: icon,
+      marginRight: theme.spacing.sm,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+    },
+
+    badge: {
+      padding: 0,
+      width: 20,
+      height: 20,
+      pointerEvents: 'none',
+    },
+  };
+});
 
 export default useStyles;
