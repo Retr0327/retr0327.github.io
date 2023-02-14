5f586096a5a27d8030509a6dcc9c12ed35d97ef1
Retr0327 <philcoke35@gmail.com>
2023-02-14 14:52:24 +0800

    Make padding customizable

--- src/components/pages/Blog/Post/Components/Image/index.tsx
+++ src/components/pages/Blog/Post/Components/Image/index.tsx
@@ -2,11 +2,16 @@ import { memo } from 'react';
 import { Image as MantineImage, AspectRatio } from '@mantine/core';
 
 function Image(props: any) {
-  const width = new URLSearchParams(new URL(props.src).search).get('w');
+  const params = new URLSearchParams(new URL(props.src).search);
+  const width = params.get('w');
+  const pt = params.get('pt');
+  const pb = params.get('pb');
   const maxWidth = width === null ? 550 : Number(width);
+  const paddingTop = pt === null ? undefined : Number(pt);
+  const paddingBottom = pb === null ? undefined : Number(pb);
 
   return (
-    <AspectRatio ratio={16 / 9} sx={{ maxWidth }}>
+    <AspectRatio ratio={16 / 9} sx={{ maxWidth, paddingTop, paddingBottom }}>
       <MantineImage {...props} />
     </AspectRatio>
   );
