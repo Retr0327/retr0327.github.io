94e40b4cffa4b3a168f274a946fff67e32649aba
Retr0327 <philcoke35@gmail.com>
2023-02-14 15:05:07 +0800

    Add MDXComponents type

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -32,3 +32,17 @@ export interface Frontmatter {
 }
 
 export type FrontMatterProps = { frontMatter: Frontmatter };
+
+type FunctionComponent<Props> = (props: Props) => JSX.Element | null;
+type ClassComponent<Props> = new (props: Props) => JSX.ElementClass;
+type Component<Props> = FunctionComponent<Props> | ClassComponent<Props>;
+
+interface NestedMDXComponents {
+  [key: string]: NestedMDXComponents | Component<any> | keyof JSX.IntrinsicElements;
+}
+
+export type MDXComponents = NestedMDXComponents & {
+  [Key in keyof JSX.IntrinsicElements]?:
+    | Component<JSX.IntrinsicElements[Key]>
+    | keyof JSX.IntrinsicElements;
+};
