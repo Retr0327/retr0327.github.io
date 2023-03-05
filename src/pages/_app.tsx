f8c3f4369c8e333e2d5b6c2594c457ad194fe742
Retr0327 <philcoke35@gmail.com>
2023-03-05 20:58:26 +0800

    Refactor layout logic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,23 @@
-import Script from 'next/script';
-import Head from 'next/head';
+import dynamic from 'next/dynamic';
 import { getCookie } from 'cookies-next';
-import { AppPropsWithLayout } from 'types';
+import { AppPropsWithControl } from 'types';
 import { ColorScheme } from '@mantine/core';
 import NextApp, { AppContext } from 'next/app';
 import MantineProvider from '@contexts/Mantine';
 
-type Props = AppPropsWithLayout & { colorScheme: ColorScheme };
+const Layout = dynamic(() => import('@components/layout'));
+
+type Props = AppPropsWithControl & { colorScheme: ColorScheme };
 
 function App(props: Props) {
   const { Component, pageProps, colorScheme } = props;
-  const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <>
-      <Head>
-        <title>Lixing Yang | Retr0327</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SF3HJM8QQL" />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-SF3HJM8QQL');
-        `}
-      </Script>
-      <MantineProvider colorScheme={colorScheme}>
-        {getLayout(<Component {...pageProps} />)}
-      </MantineProvider>
-    </>
+    <MantineProvider colorScheme={colorScheme}>
+      <Layout {...Component.control}>
+        <Component {...pageProps} />
+      </Layout>
+    </MantineProvider>
   );
 }
 
