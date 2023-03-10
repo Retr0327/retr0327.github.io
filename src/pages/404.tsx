4e4c51a1f29171bdda7069267178a85465bf1e32
Retr0327 <philcoke35@gmail.com>
2023-03-10 16:17:28 +0800

    Remove per-page layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,20 +9,18 @@ import HomeLayout from '@components/layout/Home';
 const ErrorPage = dynamic(() => import('@components/pages/Error'));
 
 const NotFoundPage: NextPageWithControl = () => (
-  <ErrorPage
-    code={404}
-    title="You have found a secret place."
-    description="Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has been moved to another URL."
-    button={
-      <Button variant="outline" component={Link} href={Route.home} size="md">
-        Take me back to home page
-      </Button>
-    }
-  />
+  <HomeLayout>
+    <ErrorPage
+      code={404}
+      title="You have found a secret place."
+      description="Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has been moved to another URL."
+      button={
+        <Button variant="outline" component={Link} href={Route.home} size="md">
+          Take me back to home page
+        </Button>
+      }
+    />
+  </HomeLayout>
 );
-
-NotFoundPage.control = {
-  Layout: (props) => <HomeLayout>{props.children}</HomeLayout>,
-};
 
 export default memo(NotFoundPage);
