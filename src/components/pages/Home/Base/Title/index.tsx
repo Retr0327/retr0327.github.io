93254e56e37dea279ae3f6a4bfae8eac12b56d6a
Retr0327 <philcoke35@gmail.com>
2023-03-15 00:35:23 +0800

    Add Title

---
+++ src/components/pages/Home/Base/Title/index.tsx
@@ -0,0 +1,16 @@
+import useStyles from './Title.styles';
+
+type Props = React.ComponentProps<'h1'>;
+
+function SectionTitle(props: Props) {
+  const { children, className, ...rest } = props;
+  const { classes, cx } = useStyles();
+
+  return (
+    <h2 className={cx(classes.title, classes.default, className)} {...rest}>
+      {children}
+    </h2>
+  );
+}
+
+export default SectionTitle;
