7c78b9ca92dbfd461b443f726cffe692bd5356de
Retr0327 <retr0327.dev@gmail.com>
2023-10-24 00:51:33 +0800

    Export header control-related components

---
+++ src/components/Navigation/Header/Controls/Icons/index.ts
@@ -0,0 +1,7 @@
+import ColorSchemeControl from './ColorScheme';
+import Facebook from './Facebook';
+import GitHub from './GitHub';
+import LinkedIn from './LinkedIn';
+import SearchMobileControl from './Search';
+
+export { ColorSchemeControl, Facebook, GitHub, LinkedIn, SearchMobileControl };
