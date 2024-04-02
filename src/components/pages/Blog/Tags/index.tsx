4c23b84739a910085fa760e1c95bab17de8b91f7
Retr0327 <retr0327.dev@gmail.com>
2024-04-02 22:25:37 +0800

    Implement conditional rendering for word cloud and archives

--- src/components/pages/Blog/Tags/index.tsx
+++ src/components/pages/Blog/Tags/index.tsx
@@ -1,62 +1,21 @@
-import Wordcloud from '@visx/wordcloud/lib/Wordcloud';
-import { Text } from '@visx/text';
-import { Route } from '@config';
-import { useRouter } from 'next/navigation';
-import { scaleLog } from '@visx/scale';
-import { Stack } from '@mantine/core';
-import { countWordFreq, WordData } from './utils';
-import classes from './BlogTags.module.css';
+import { useSearchParams, redirect } from 'next/navigation';
+import BlogTagArchives from './Archives';
+import WordCloud from './WordCloud';
 
-interface BlogTagPageProps {
-  tags: string[];
-  showControls?: boolean;
-}
+function BlogTagPage() {
+  const tag = useSearchParams().get('tag');
+  const page = useSearchParams().get('page');
 
-const COLORS = ['#143059', '#2F6B9A', '#82a6c2'];
+  if (tag === null && page === null) {
+    return <WordCloud />;
+  }
 
-function BlogTagPage(props: BlogTagPageProps) {
-  const words = countWordFreq(props.tags);
-  const router = useRouter();
-  const setFontSize = (datum: WordData) =>
-    scaleLog({
-      domain: [Math.min(...words.map((w) => w.value)), Math.max(...words.map((w) => w.value))],
-      range: [10, 100],
-    })(datum.value);
+  if (tag === null) {
+    return redirect('/404');
+  }
 
-  return (
-    <div className={classes.container}>
-      <Stack align="center" gap={50}>
-        <Wordcloud
-          words={words}
-          width={800}
-          height={400}
-          fontSize={setFontSize}
-          font="Impact"
-          padding={2}
-          spiral="archimedean"
-          rotate={0}
-          random={() => 0.5}
-        >
-          {(cloudWords) =>
-            cloudWords.map((word, index) => (
-              <Text
-                key={`${word.text}-${index}`}
-                fill={COLORS[index % COLORS.length]}
-                textAnchor="middle"
-                transform={`translate(${word.x}, ${word.y}) rotate(${word.rotate})`}
-                fontSize={word.size}
-                fontFamily={word.font}
-                className={classes.cloudWord}
-                onClick={() => router.push(`${Route.Tags}/${encodeURI(word.text!)}?page=1`)}
-              >
-                {word.text}
-              </Text>
-            ))
-          }
-        </Wordcloud>
-      </Stack>
-    </div>
-  );
+  const tagPage = page === null ? 1 : Number(page);
+  return <BlogTagArchives tag={tag} page={tagPage} />;
 }
 
 export default BlogTagPage;
