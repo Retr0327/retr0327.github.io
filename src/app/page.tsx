58572a202dd4889ed1a16e6d79286410fd57dbd5
Retr0327 <retr0327.dev@gmail.com>
2024-04-29 14:21:09 +0800

    Simplify the JSX structure by removing an unnecessary React fragment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,24 @@
 import dynamic from 'next/dynamic';
-import Jumbotron from '@components/pages/Home/Jumbotron';
-import Waves from '@components/pages/Home/Waves/Waves';
+import Jumbotron from '@views/Home/Jumbotron';
+import Waves from '@views/Home/Waves/Waves';
 
-const Experiences = dynamic(() => import('@components/pages/Home/Experiences'));
+const Experiences = dynamic(() => import('@views/Home/Experiences'));
 
 function Home() {
   return (
-    <>
-      <div
-        style={{
-          position: 'relative',
-          zIndex: 1,
-          boxShadow: 'var(--mantine-shadow-md)',
-          backgroundColor: 'var(--mantine-color-body)',
-          minHeight: '100vh',
-        }}
-      >
-        <Jumbotron />
-        <Waves height={40} width={150} />
-        <Experiences />
-      </div>
-    </>
+    <div
+      style={{
+        position: 'relative',
+        zIndex: 1,
+        boxShadow: 'var(--mantine-shadow-md)',
+        backgroundColor: 'var(--mantine-color-body)',
+        minHeight: '100vh',
+      }}
+    >
+      <Jumbotron />
+      <Waves height={40} width={150} />
+      <Experiences />
+    </div>
   );
 }
 
