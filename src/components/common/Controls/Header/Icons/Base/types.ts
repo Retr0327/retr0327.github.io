0d7f5db413ea79d4a1367f24e0ca14a45e7bc80e
Retr0327 <retr0327.dev@gmail.com>
2024-04-29 15:06:27 +0800

    Add Greycliff-related WOFF2 fonts

--- src/components/common/Controls/Header/Icons/Base/types.ts
+++
@@ -1,7 +0,0 @@
-import type { BoxProps } from '@mantine/core';
-
-export interface HeaderControlProps extends BoxProps {
-  tooltip: string;
-  'aria-label'?: string;
-  children: React.ReactNode;
-}
--- src/components/common/Controls/Header/Icons/index.tsx
+++
@@ -1,7 +0,0 @@
-import ColorSchemeControl from './ColorScheme';
-import FacebookControl from './Facebook';
-import GithubControl from './Github';
-import LinkedInControl from './LinkedIn';
-import SearchMobileControl from './Search';
-
-export { ColorSchemeControl, FacebookControl, GithubControl, LinkedInControl, SearchMobileControl };
