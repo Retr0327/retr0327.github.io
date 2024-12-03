ef970e84b62423d440fbcce43ee4eb4785188dbd
retr0327 <retr0.yang@linego.me>
2024-12-03 15:38:41 +0800

    feat: implement posts metadata retrieval utility

--- src/services/mdx.ts
+++ src/services/mdx.ts
@@ -5,7 +5,7 @@ import { PostMetadataType } from 'types/mdx';
 const POSTS_PATH = path.join(process.cwd(), 'src/app/blog/p');
 const POST_FOLDERS = fs.readdirSync(POSTS_PATH).filter((file) => file);
 
-async function getPostsMetadata() {
+export async function getPostsMetadata() {
   const posts: PostMetadataType[] = [];
   for (const folder of POST_FOLDERS) {
     const { metadata } = await import(`src/app/blog/p/${folder}/page.mdx`);
@@ -18,5 +18,3 @@ async function getPostsMetadata() {
       new Date(a.updatedAt || a.createdAt).getTime()
   );
 }
-
-export { POSTS_PATH, POST_FOLDERS, getPostsMetadata };
