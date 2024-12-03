eb3b7e8e5ce6747f9786ce8710e50522c23ad721
retr0327 <retr0.yang@linego.me>
2024-12-03 15:39:39 +0800

    feat: add LINE GO work experience

--- src/config/experiences.ts
+++ src/config/experiences.ts
@@ -1,11 +1,20 @@
 export const EXPERIENCES = [
   {
-    title: 'Backend Engineer',
+    title: 'Senior Backend Engineer',
+    image:
+      'https://play-lh.googleusercontent.com/SDFzkF4HUPQVoU6s2xYrokB4E4YM1omoAS-jXOwrYqwdYAghbhDkNkwphy1qUsoxQXw=w240-h480-rw',
+    company: 'LINE GO',
+    url: 'https://www.linego.me/',
+    start: 'Aug 2024',
+    end: null,
+  },
+  {
+    title: 'Fullstack Engineer',
     image:
       'https://lh3.googleusercontent.com/a-/ACB-R5Q3ydtUuw9m4ABDeIUJk0zn0MWVQqKmr7UqsPEE=s40-p',
     company: 'TRAPA Security',
     url: 'https://trapasecurity.com/',
     start: 'Apr 2023',
-    end: null,
+    end: 'July 2024',
   },
 ] as const;
