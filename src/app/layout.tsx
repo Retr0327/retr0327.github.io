b3c344d4c9ebc4ce7e62d6a6ed0fda9feb18010d
retr0327 <retr0.yang@linego.me>
2024-12-03 15:44:20 +0800

    refactor: remove MDX provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,42 +2,34 @@ import '@mantine/code-highlight/styles.css';
 import '@mantine/core/styles.css';
 import '@mantine/spotlight/styles.css';
 
-import theme from '@config/theme';
-import type { Metadata, Viewport } from 'next';
+import type { Metadata } from 'next';
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import FontsStyle from '@components/Fonts';
 import GaScript from '@components/GAScript';
-import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import Shell from '@components/Shell';
-import SpotlightProvider from '@contexts/Spotlight';
-import { getPostsMetadata } from '@services/mdx';
-import { MdxMetadataProvider } from '@contexts/Mdx';
+import Spotlight from '@components/Spotlight';
+import theme from '@config/theme';
 
 export const metadata: Metadata = {
   title: 'Lixing Yang | Retr0',
 };
 
-export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  userScalable: false,
-};
-
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const postsMetadata = await getPostsMetadata();
   return (
-    <html lang="en" translate="no">
+    <html lang="en" translate="no" suppressHydrationWarning>
       <head>
         <ColorSchemeScript />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
       </head>
       <GaScript />
       <FontsStyle />
       <body>
         <MantineProvider theme={theme}>
-          <MdxMetadataProvider metadata={postsMetadata}>
-            <SpotlightProvider>
-              <Shell>{children}</Shell>
-            </SpotlightProvider>
-          </MdxMetadataProvider>
+          <Spotlight />
+          <Shell>{children}</Shell>
         </MantineProvider>
       </body>
     </html>
