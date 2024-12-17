1cc1e5ea05209115f53186511c461816f5ef71bc
Retr0327 <retr0.yang@linego.me>
2024-12-17 14:50:10 +0800

    refactor: switch to async file system operations

--- src/services/mdx.ts
+++ src/services/mdx.ts
@@ -1,13 +1,14 @@
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 import { PostMetadataType } from 'types/mdx';
 
 const POSTS_PATH = path.join(process.cwd(), 'src/app/blog/p');
-const POST_FOLDERS = fs.readdirSync(POSTS_PATH).filter((file) => file);
 
 export async function getPostsMetadata() {
+  const postFolders = await fs.readdir(POSTS_PATH);
+  const folders = postFolders.filter((file) => file);
   const posts: PostMetadataType[] = [];
-  for (const folder of POST_FOLDERS) {
+  for (const folder of folders) {
     const { metadata } = await import(`src/app/blog/p/${folder}/page.mdx`);
     posts.push(metadata);
   }
