caef3731ad2a062a47b3a8773d09be556b301261
Retr0327 <retr0.yang@linego.me>
2024-12-17 15:13:32 +0800

    refactor: update personal information and title

--- src/views/Home/Jumbotron/Avatar/index.tsx
+++ src/views/Home/Jumbotron/Avatar/index.tsx
@@ -1,16 +1,16 @@
-import { Avatar as MantineAvatar, rem, Text, Box, Group } from '@mantine/core';
+import { Box, Group, Avatar as MantineAvatar, rem, Text } from '@mantine/core';
 import { Href } from '@config';
-import { Github, LinkedIn, Email, CV } from './Icons';
+import { CV, Email, Github, LinkedIn } from './Icons';
 
 function Avatar() {
   return (
     <Box>
       <MantineAvatar src={Href.Avatar} size={250} radius={120} mx="auto" />
       <Text ta="center" style={{ fontSize: rem(30) }} fw={500} mt="md">
-        Lixing Yang
+        Retr0 Yang
       </Text>
       <Text ta="center" c="dimmed" style={{ fontSize: rem(20) }}>
-        Fullstack developer
+        Senior Backend Engineer
       </Text>
       <Group gap={10} justify="center" mt="lg">
         <CV />
